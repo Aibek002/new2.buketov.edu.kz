4e40fbdcb2040e25d3e1c18232d070373b27975a
Aibek002 <200103346@stu.sdu.edu.kz>
2025-06-09 23:38:27 +0500

    modified:   views/site/admission.php 	modified:   web/css/admission.css 	modified:   web/js/admission.js 	new file:   web/pdf/admission/file1.pdf

--- web/js/admission.js
+++ web/js/admission.js
@@ -1,17 +1,29 @@
 function openSection(type) {
-    const existsClasses = document.querySelectorAll('.bakalavriat, .magistrant, .doctorant');
-    existsClasses.forEach((classElement) => {
-        classElement.classList.remove('active');
-    });
+  const existsClasses = document.querySelectorAll(
+    ".bakalavriat, .magistrant, .doctorant"
+  );
+  existsClasses.forEach((classElement) => {
+    classElement.classList.remove("active");
+  });
 
-    if (type == 'bakalavr') {
-        const bakalavr = document.querySelector(".bakalavriat");
-        bakalavr.classList.toggle('active');
-    }else if(type == 'magistrant'){
-        const magistrant = document.querySelector(".magistrant");
-        magistrant.classList.toggle('active');
-    }else{
-        const doctorant = document.querySelector(".doctorant");
-        doctorant.classList.toggle('active');
-    }
+  if (type == "bakalavr") {
+    const bakalavr = document.querySelector(".bakalavriat");
+    bakalavr.classList.toggle("active");
+  } else if (type == "magistrant") {
+    const magistrant = document.querySelector(".magistrant");
+    magistrant.classList.toggle("active");
+  } else {
+    const doctorant = document.querySelector(".doctorant");
+    doctorant.classList.toggle("active");
+  }
+}
+function openGeneralRulesPdf(filePath) {
+  const general_rules_pdf = document.querySelector(".general-rules-pdf");
+  general_rules_pdf.src = "";
+
+  setTimeout(() => {
+    general_rules_pdf.classList.toggle("active");
+    console.log(filePath);
+    general_rules_pdf.src = filePath;
+  }, 50);
 }
