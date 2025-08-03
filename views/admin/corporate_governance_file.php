7998c31016d410ec72f843f85cef3ede40bf4e2a
Aibek002 <200103346@stu.sdu.edu.kz>
2025-08-03 13:31:07 +0500

    modified:   views/admin/corporate_governance_file.php 	modified:   web/css/corporate-admin-panel.css 	modified:   web/js/corporate-admin-panel-sort.js

--- views/admin/corporate_governance_file.php
+++ views/admin/corporate_governance_file.php
@@ -46,7 +46,7 @@ $this->title = "Управление файлами Корпоративного
         ->label(false);
     ?>
     <?= $form->field($model, 'file')->fileInput(['class' => 'select-file', 'placeholder' => 'Выберите файл'])->label(false) ?>
-    <?= $form->field($model, 'name_url')->textInput(['class' => 'name_url_input', 'placeholder' => 'Название ссылки'])->label(false); ?>
-
+    <?= $form->field($model, 'name_url')->textInput(['class' => 'form-control name_url_input', 'placeholder' => 'Название ссылки'])->label(false); ?>
+    <?= Html::submitButton('Submit', ['class' => 'submitButton btn btn-success']) ?>
     <?php ActiveForm::end() ?>
 </div>
