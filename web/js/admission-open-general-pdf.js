0f1575aac3484f4084bc16f0354e11a13ca2953d
Aibek002 <200103346@stu.sdu.edu.kz>
2025-09-10 15:38:30 +0500

    deleted:    controllers/04.11.2020.pdf 	modified:   views/site/admission.php 	modified:   web/js/admission-open-general-pdf.js

--- web/js/admission-open-general-pdf.js
+++ web/js/admission-open-general-pdf.js
@@ -19,3 +19,45 @@ function openGeneralRulesPdf(filePath, type) {
     // Прокрутка к элементу
     general_rules_pdf.scrollIntoView({ behavior: 'smooth' });
 }
+function openGeneralRulesPdfMagistr(filePath, type) {
+    let selector = '.general-rules-pdf-magistr'; // Это класс
+    const general_rules_pdf = document.querySelector(selector); // получаем элемент по классу
+
+    if (!general_rules_pdf) {
+        console.warn("Элемент не найден:", selector);
+        return;
+    }
+
+    // Очистка и обновление источника PDF
+    general_rules_pdf.src = "";
+    general_rules_pdf.classList.remove("active");
+
+    setTimeout(() => {
+        general_rules_pdf.classList.add("active");
+        general_rules_pdf.src = filePath;
+    }, 50);
+
+    // Прокрутка к элементу
+    general_rules_pdf.scrollIntoView({ behavior: 'smooth' });
+}
+function openGeneralRulesPdfDoctoral(filePath, type) {
+    let selector = '.general-rules-pdf-doctoral'; // Это класс
+    const general_rules_pdf = document.querySelector(selector); // получаем элемент по классу
+
+    if (!general_rules_pdf) {
+        console.warn("Элемент не найден:", selector);
+        return;
+    }
+
+    // Очистка и обновление источника PDF
+    general_rules_pdf.src = "";
+    general_rules_pdf.classList.remove("active");
+
+    setTimeout(() => {
+        general_rules_pdf.classList.add("active");
+        general_rules_pdf.src = filePath;
+    }, 50);
+
+    // Прокрутка к элементу
+    general_rules_pdf.scrollIntoView({ behavior: 'smooth' });
+}
